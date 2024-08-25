b1b699affc51ccc0052aae6055ccbeda5cf1545c
Chris-AM <c.aranguizm@outlook.com>
2024-08-24 22:17:52 -0400

    :sparkles: declaring course card component and using stand alone components

---
+++ src/app.config.ts
@@ -0,0 +1,13 @@
+import { provideRouter } from "@angular/router";
+import { ApplicationConfig } from "@angular/core";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+  ],
+};
